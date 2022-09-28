b24c61eb53b5eb0036ea6f6f1232a5730d923346
xie14 <114558724+xie14@users.noreply.github.com>
2022-09-28 14:19:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -60,7 +60,7 @@ const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: 'oaQp_55I6fQ_ucbaKZ2i5fw83z7U',
+  USERS: ['oaQp_55I6fQ_ucbaKZ2i5fw83z7U'],
     
       // 想要发送的人的名字
       name: '老婆0',
