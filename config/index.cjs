a9a3070d6aadffe6551b504cc70c71b9edda12b1
xie14 <114558724+xie14@users.noreply.github.com>
2022-09-28 13:55:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: 'wxb1755b9e50f8d3b1',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET: '4c2f73c0f030590daef38732013c1f15',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
